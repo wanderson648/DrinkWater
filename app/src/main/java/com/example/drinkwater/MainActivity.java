eb1f564f6921bec4865a7b354f93db1e0fc05aa3
wanderson648 <testepedroca@gmail.com>
2022-04-20 22:44:10 -0300

    armazenando dados em um banco local

--- app/src/main/java/com/example/drinkwater/MainActivity.java
+++ app/src/main/java/com/example/drinkwater/MainActivity.java
@@ -1,50 +1,105 @@
 package com.example.drinkwater;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnNotify;
-    private EditText editMinutes;
-    private TimePicker timePicker;
+  private Button btnNotify;
+  private EditText editMinutes;
+  private TimePicker timePicker;
 
-    private int hour;
-    private int minutes;
-    private int interval;
+  private int hour;
+  private int minutes;
+  private int interval;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
-      setContentView(R.layout.activity_main);
+  private boolean activated;
 
-      // pegando os ids definidos no xml
-      btnNotify = findViewById(R.id.btn_notify);
-      editMinutes = findViewById(R.id.edit_text_number_interval);
-      timePicker = findViewById(R.id.time_picker);
+  private SharedPreferences preferences;
 
 
-      // definindo o formato de hora no padrão brasileiro
-      timePicker.setIs24HourView(true);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
+    btnNotify = findViewById(R.id.btn_notify);
+    editMinutes = findViewById(R.id.edit_text_number_interval);
+    timePicker = findViewById(R.id.time_picker);
+
+    timePicker.setIs24HourView(true);
+    preferences = getSharedPreferences("db", Context.MODE_PRIVATE);
+
+    activated = preferences.getBoolean("activated", false);
+
+    if(activated) {
+      btnNotify.setText(R.string.pause);
+      int color = ContextCompat.getColor(this, R.color.teal_700);
+      btnNotify.setBackgroundColor(color);
+
+      int interval = preferences.getInt("interval", 0);
+      int hour = preferences.getInt("hour", timePicker.getCurrentHour());
+      int minute = preferences.getInt("minutes", timePicker.getCurrentMinute());
+
+      editMinutes.setText(String.valueOf(interval));
+      timePicker.setCurrentHour(hour);
+      timePicker.setCurrentMinute(minute);
+    }
+  }
+
+
+
+  public void notifyClick(View view) {
+    String sInterval = editMinutes.getText().toString();
+
+    if (sInterval.isEmpty()) {
+      Toast.makeText(this, R.string.error_msg, Toast.LENGTH_SHORT).show();
+      return;
     }
 
-    // forma simples de ouvir eventos de touch
-    public void notifyClick(View view) {
-      String sInterval = editMinutes.getText().toString();
+    hour = timePicker.getCurrentHour();
+    minutes = timePicker.getCurrentMinute();
+    interval = Integer.parseInt(sInterval);
 
-      hour = timePicker.getCurrentHour();
-      minutes = timePicker.getCurrentMinute();
-      interval = Integer.parseInt(sInterval);
+    if(!activated) {
+      btnNotify.setText(R.string.pause);
+      int color = ContextCompat.getColor(this, R.color.teal_700);
+      btnNotify.setBackgroundColor(color);
+      activated = true;
 
-      Log.d("teste", "horas: " + hour + "minutes: "+ minutes + " intervalo: "+ interval);
+      SharedPreferences.Editor editor = preferences.edit();
+      editor.putBoolean("activated", true);
+      editor.putInt("hour", hour);
+      editor.putInt("minutes", minutes);
+      editor.putInt("interval", interval);
+      editor.apply();
+
+
+    } else {
+      btnNotify.setText(R.string.notify);
+      int color = ContextCompat.getColor(this, androidx.cardview.R.color.cardview_dark_background);
+      btnNotify.setBackgroundColor(color);
+      activated = false;
+
+      SharedPreferences.Editor editor = preferences.edit();
+      editor.putBoolean("activated", false);
+      editor.remove("hour");
+      editor.remove("minutes");
+      editor.remove("interval");
+      editor.apply();
     }
+
+  }
 }
 
 
